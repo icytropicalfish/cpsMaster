4245ac5db8d4a66c7c0eef1b46a4b0e21eb13c24
Arodios <acorn666hy@gmail.com>
2024-07-18 20:54:07 +1200

    double cps done

---
+++ src/main.py
@@ -0,0 +1,41 @@
+import keyboard
+import pyautogui
+import time
+import win32api
+import win32con
+
+def detect_mouse_button4():
+    while True:
+        if win32api.GetKeyState(win32con.VK_XBUTTON1) < 0:
+            return
+        time.sleep(0.01)
+
+def detect_left_click():
+    while True:
+        if win32api.GetKeyState(win32con.VK_XBUTTON1) < 0:
+            print("开始检测左键点击")
+            while True:
+                if win32api.GetKeyState(win32con.VK_LBUTTON) < 0:
+                    pyautogui.click()
+                    print("增加一个cps")
+                    time.sleep(0.1)
+                
+                if keyboard.is_pressed(']'):
+                    print("程序终止")
+                    exit(0)
+                
+                time.sleep(0.01)
+
+def on_key_event(event):
+    if event.name not in ['space', 'w', 'a', 's', 'd', 'shift'] and event.event_type == 'down':
+        print("暂停检测")
+        return True
+    return False
+
+if __name__ == "__main__":
+    print("启动程序")
+    while True:
+        detect_mouse_button4()
+        keyboard.hook(on_key_event)
+        detect_left_click()
+        keyboard.unhook_all()
